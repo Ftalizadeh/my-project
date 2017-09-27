0c50b2677a41e779cef60384af4950410702ff7f
fa <ft.alizadeh.1395@gmail.com>
2017-09-27 14:38:59 +0330

    add my project

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Hassan-Shakeri</title>
+<meta name="keywords" content="single, slider, free templates, website templates, CSS, HTML" />
+<meta name="description" content="Single Slider is a free CSS template provided by templatemo.com" />
+<link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
+
+<script src="js/jquery-1.2.6.min.js" type="text/javascript"></script>
+<script src="js/jquery.easing.1.3.js" type="text/javascript"></script>
+<script src="js/jquery.kwicks-1.5.1.pack.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+	$().ready(function() {  
+		$('.kwicks').kwicks({  
+			max : 710,  
+			spacing : 0,  
+			sticky: true,
+		});  
+	}); 
+</script>
+<script type="text/javascript" src="js/jquery-1-4-2.min.js"></script> 
+<link rel="stylesheet" href="css/slimbox2.css" type="text/css" media="screen" /> 
+<script type="text/JavaScript" src="js/slimbox2.js"></script> 
+
+</head>
+
+<body>
+
+<div id="templatemo_wrapper">
+	<div id="templatemo_main">
+	
+		<div id="site_title">
+			<h1><a href="http://www.templatemo.com"></a></h1>
+		</div>
+		
+		<div id="templatemo_content">
+			<ul class="kwicks">
+			
+				<li id="home"><span class="header"></span>
+					<div class="inner">
+						<h2>Welcome to Hassan Shakeri Website</h2>
+						<p><em>I was born in mashhad,on month day,years.I have a bachelor's degree in computer engineering -software-ferdowsi-university of mashhad 1995 & mater's degree in computer engineering -software-shrif university of technology 1997 & PH.D degree computer inengineering -software-ferdowsi-university of mashhad</em></p>
+						<img src="images/Picture Shakeri.jpg" alt="Image 01" class="image_fl" />
+						<p><em><div class="col_half float_r">
+							<h3>Master's Thesis </h3>
+							<ul class="templatemo_list">
+								<li>Design and implementation of network management software Sharif</li>
+							</ul>
+						</div></em></p>
+						<p><em><div class="col_half float_r">
+							<h3>Doctoral Dissertation </h3>
+							<ul class="templatemo_list">
+								<li>Presents a computational model based on the trust management</li>
+							</ul>
+						</div></em></p>
+                        <p><em><div class="col_half float_r">
+							<h3>Research areas of interest </h3>
+							<ul class="templatemo_list">
+								<li>( Natural Language Processing)NLP</li>
+                                <li>(Computer System Security) </li>
+                                <li>(Trust Management)</li>
+                                <li>(Semantic Web)</li>
+							</ul>
+						</div></em></p>
+					</div>
+				</li>
+			
+				<li id="about"><span class="header"></span>
+					<div class="inner">
+						<h2>About</h2>
+						<!--<img src="images/templatemo_image_02.jpg" alt="Image 02" class="image_fr" />-->
+						<p><em><div class="col_half float_r">
+							<h3>Experiences and lessons:</h3>
+							<ul class="templatemo_list">
+								<li>Member of the Scientific Board of Islamic Azad University of Mashhad in 1998 so far</li>
+                                <li>Teaching the following undergraduate courses</li>
+                                <li> Computer Networks</li>
+                                <li>Data Structure</li>
+                                <li>Discrete Mathematics</li>
+                                <li> Advanced Programming</li>
+                                <li>Algorithm design</li>
+                                <li>Fundamentals of Computer Programming</li>
+							</ul>
+						</div></em></p>
+                       	<p><em><div class="col_half float_r">
+							<h3>Executive and administrative records:</h3>
+							<ul class="templatemo_list">
+								<li>First Director of Information Technology Department of Islamic Azad University of Mashhad,  2005 to 2009</li>
+                                <li>Manager of Engineering, Islamic Azad University of Mashhad, 2007 to 2009</li>
+                                </ul>
+                                <h3>Research Projects:</h3>
+							      <ul class="templatemo_list">
+                                <li>Run the project as open networks based on NetWare, Windows, and UNIX, Islamic Azad University of Mashhad, 2002.</li>
+                                <li>Run the project as the application of automatic text summarization Persian, Islamic Azad University of Mashhad, 2010.</li>
+
+							</ul>
+						</div></em></p>
+					
+					</div>
+				</li>
+			
+				<li id="social"><span class="header"></span>
+					<div class="inner">
+						<h2>Social</h2>
+						<p><em>Establish Relationship Whit Website Link Functional.</em></p>
+						<ul class="social_links">
+							<li><a href="Resume for Site.docx" class="resum">resum</a></li>
+							<li><a href="users/index.php" class="PanelMembers">Panel Members</a></li>
+							<li><a href="form.php" class="Membership">Membership</a></li> 
+                            <!--<li><a href="#" class="news">news</a></li>-->
+							<li><a href="http://itsa.mshdiau.ac.ir/index.php" class="it">IT Community </a></li>
+                            <li><a href="http://mshdiau.ac.ir/" class="azad">Islamic Azad University</a></li>
+						</ul>
+					</div>
+				</li>
+			
+				<li id="portfolio"><span class="header"></span>
+					<div class="inner">
+						<h2>Portfolio</h2>
+						<ul id="gallery">
+							<li><a href="images/portfolio/01.jpg" rel="lightbox[portfolio]" title=""><img src="images/portfolio/01-l.jpg" alt="Image 01" /></a></li>
+							<!--<li><a href="images/portfolio/02-l.jpg" rel="lightbox[portfolio]" title="Nulla varius laoreet tellus, non volutpat mi iaculis a."><img src="images/portfolio/02.jpg" alt="Image 02" /></a></li>
+							<li><a href="images/portfolio/03-l.jpg" class="no_margin_right" rel="lightbox[portfolio]" title="Vestibulum euismod mi et massa volutpat lobortis."><img src="images/portfolio/03.jpg" alt="Image 03" /></a></li>
+							<li><a href="images/portfolio/04-l.jpg" rel="lightbox[portfolio]" title="Quisque volutpat nunc a felis lobortis aliquet."><img src="images/portfolio/04.jpg" alt="Image 04" /></a></li>
+							<li><a href="images/portfolio/05-l.jpg" rel="lightbox[portfolio]" title="Nullam dictum enim et quam posuere dapibus."><img src="images/portfolio/05.jpg" alt="Image 05" /></a></li>
+							<li><a href="images/portfolio/06-l.jpg" class="no_margin_right" rel="lightbox[portfolio]" title="Phasellus condimentum nisi et mi feugiat dapibus."><img src="images/portfolio/06.jpg" alt="Image 06" /></a></li>-->
+						</ul>
+					</div>
+				</li>
+				
+				<li id="contact"><span class="header"></span>
+					<div class="inner">
+						<h2>Contact Me</h2>
+						<p>Please fill in all the fields; I will answer you as soon as possible. Also you can contact me directly by Hassan.shakeri@gmail.com.</p>
+						<h4>Send Me a message!</h4>
+						<div id="contact_form"  class="col_w280 float_l">
+							<form method="post" name="contact" action="#">
+							
+								<label for="author">subject:</label> <input type="text" id="author" name="subject" class="required input_field" />
+								<div class="cleaner h10"></div>
+								<label for="email">your Email:</label> <input type="text" id="email" name="email" class="validate-email required input_field"  />
+								<div class="cleaner h10"></div>
+				
+								<label for="text">Message:</label> <textarea id="text" name="message" rows="0" cols="0" class="required"></textarea>
+								<div class="cleaner h10"></div>
+								
+								<input type="submit" value="Send" id="submit" name="submit" class="" />
+								
+								
+							</form>
+						</div>
+                        
+                        <div class="col_w280 float_r">
+                            <h4>Mailing Address</h4>
+                            
+                            <strong>Phone:</strong> : 0511 - 6622789<br />          
+                            <strong>Email:</strong> shakeri@mshdiau.ac.ir
+               			</div>
+						
+					</div>
+				</li>
+			
+			</ul>
+			
+		</div> <!-- END of content -->
+	</div> <!-- END of templatemo_main -->
+    <div id="templatemo_footer"> desining by fatemeh alizadeh<a href="http://www.templatemo.com" target="_parent"></a></div>
+</div> <!-- END of templatemo_wrapper -->
+
+</body>
+</html>
